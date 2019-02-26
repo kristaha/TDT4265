e2ed492b6f92679e88a247c1fcf4b0d8dabccf93
Kristian Haga <kristianhaga@Kristians-MacBook-Pro.local>
2019-02-26 14:31:25 +0100

    FINISHED

--- Assignment_3/code/task3_visualisation.py
+++ Assignment_3/code/task3_visualisation.py
@@ -7,7 +7,9 @@ from torch import nn
 import matplotlib.pyplot as plt
 import math
 
-img_url = 'https://s3.amazonaws.com/outcome-blog/wp-content/uploads/2017/02/25192225/cat.jpg'
+#img_url = 'https://s3.amazonaws.com/outcome-blog/wp-content/uploads/2017/02/25192225/cat.jpg' #Cat
+#img_url = 'http://expertofdreams.com/data_images/zebra/zebra-4.jpg' # Zebra
+img_url = 'https://i.ytimg.com/vi/cgHZYsFP2cE/maxresdefault.jpg' # G-wagon
 
 def load_and_preprocess_img():
     response = requests.get(img_url)
@@ -17,63 +19,83 @@ def load_and_preprocess_img():
         mean = [0.4914, 0.4822, 0.4465],
         std = [0.2023, 0.1994, 0.2010]
         )
-    preprocess = transforms.Compose([
+    preprocess_and_normalize = transforms.Compose([
         transforms.Resize(256),
-        #transforms.CenterCrop(224),
         transforms.CenterCrop(256),
         transforms.ToTensor(),
         normalize
         ])
 
-    img_tensor = preprocess(img_pil)
-    img_tensor.unsqueeze_(0)
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(256),
+        transforms.ToTensor(),
+        ])
+
+    img_normalized = preprocess_and_normalize(img_pil)
+    img_normalized.unsqueeze_(0)
+
+    img = preprocess(img_pil)
+    img.unsqueeze_(0)
     #return Variable(img_tensor)
-    return img_tensor
+    return img, img_normalized
 
 def plot_filters(img, activation, num_filters):
-    plt.figure(1, figsize=(10,10))
+    original_img = img[0].numpy().transpose(1,2,0) # To fit tensor into imshow(c, h, w)
     n_columns = 6
     n_rows = math.ceil(num_filters / n_columns) + 1
+    plt.figure(1, figsize=(10,10))
     plt.subplot(n_rows, n_columns, 1)
-    plt.imshow(img[0,2,:,:])
+    plt.xticks([]) # Remove labels on axis
+    plt.yticks([])
+    plt.title('Original image')
+    plt.imshow(original_img)
     for i in range(num_filters):
         plt.subplot(n_rows, n_columns, i+2)
-        #plt.title('Filter ' + str(i))
+        plt.title('Filter ' + str(i + 1))
+        plt.xticks([])
+        plt.yticks([])
         plt.imshow(activation[0,i,:,:], interpolation="nearest", cmap="gray")
 
 
-def visualize_first_conv_layer_filters_resnet18(image, num_filters):
-    activation = models.resnet18(pretrained=True).conv1(image)
+def visualize_first_conv_layer_filters_resnet18(image, image_normalized, num_filters):
+    activation = models.resnet18(pretrained=True).conv1(image_normalized)
     plot_filters(image, activation.detach().numpy(), num_filters)
 
 
 
-def visualize_last_conv_layer_filters_resnet18(image, num_filters):
+def visualize_last_conv_layer_filters_resnet18(image, image_normalized, num_filters):
     resnet18_last_conv = nn.Sequential(*list(models.resnet18(pretrained=True).children())[:-2])
 
     for param in resnet18_last_conv.parameters():
         param.requires_grad = False
 
-    output = resnet18_last_conv(image)
+    output = resnet18_last_conv(image_normalized)
 
-    plot_filters(output.detach().numpy(), num_filters)
+    plot_filters(image, output.detach().numpy(), num_filters)
 
 def visualize_weights_first_conv_layer():
     weights = models.resnet18(pretrained=True).conv1.weight.data.numpy()
-    plt.imshow(weights[0, 0, :, :])
+    weights = weights[0].transpose(1,2,0)
+    plt.title('Weights in first conv layer')
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(weights)
     
 print('Start preprocessing..')
-img = load_and_preprocess_img()
+img, img_norm = load_and_preprocess_img()
 
-#first_layer = visualize_first_conv_layer_filters_resnet18(img)
-#plt.imshow(first_layer[0, :, :, 2], cmap='gray')
-#visualize_first_conv_layer_filters_resnet18(img, 10)
-#plt.show()
+#Visualize first convolutional layer
+visualize_first_conv_layer_filters_resnet18(img, img_norm, 11)
+plt.show()
 
-#visualize_last_conv_layer_filters_resnet18(img, 10)
-#plt.show()
+#Visualize last convolutional layer
+visualize_last_conv_layer_filters_resnet18(img, img_norm, 11)
+plt.show()
 
+#Visualize weights in first convolutional layer
 visualize_weights_first_conv_layer()
 plt.show()
 #Spørsmål - hvordan printe original bilde?
-# Noe er galt med weights
+# Filter i første lag ser etter kanter og slikt
+# Siste lag er blitt så abstrahert at det programmet nå ekstraherer av features ikke gir mening for oss. 
